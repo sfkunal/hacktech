6a82f4864e1ab2515881e2e3de713e97e1a66977
sfkunal <kunal@masagroup.com>
2023-03-04 23:08:30 -0800

    new prof image

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,13 +3,13 @@ import { Image, StyleSheet, View } from 'react-native';
 import styles from "./styles";
 
 const logo = require('../assets/logo.png');
-const profilePicture = require('../assets/profile.png');
+const accPhoto = require('../assets/account_photo.png');
 
 const Header = () => {
   return (
     <View style={styles.header}>
       <Image source={logo} style={styles.logo} resizeMode='contain' />
-      <Image source={profilePicture} style={styles.profilePicture} />
+      <Image source={accPhoto} style={styles.profilePicture} />
     </View>
     
   );
